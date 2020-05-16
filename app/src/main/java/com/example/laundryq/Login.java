f4a8631946a09c0ea96dae3b774d413262aa47ad
shoesproject80 <rizky.aviansyah25@gmail.com>
2020-05-16 14:23:57 +0700

    Update

--- app/src/main/java/com/example/laundryq/Login.java
+++ app/src/main/java/com/example/laundryq/Login.java
@@ -1,11 +1,20 @@
 package com.example.laundryq;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
@@ -16,21 +25,46 @@ public class Login extends AppCompatActivity {
 
         Button rg = findViewById(R.id.regbtn);
         Button lg = findViewById(R.id.loginbtn);
-        rg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent reg = new Intent(Login.this ,Regist.class);
-                startActivity(reg);
-            }
-        });
         lg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent log = new Intent(Login.this ,Dashboard.class);
-                startActivity(log);
+                Intent rg = new Intent(Login.this, Dashboard.class);
+                startActivity(rg);
             }
         });
 
+        rg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rg = new Intent(Login.this, Regist.class);
+                startActivity(rg);
+            }
+        });
 
     }
+
+    public static void addTextChangedListener(EditText e, final TextInputLayout t) {
+        e.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    if (!TextUtils.isEmpty(t.getError())) {
+                        t.setError(null);
+                        t.setErrorEnabled(false);
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
 }
